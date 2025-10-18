0e4f66011eae3022e1102b433033903c82401cd5
insaneAsh <rathoreashutosh1011@gmail.com>
2025-10-18 11:53:41 +0530

    second commit

--- src/main/java/org/group10/tradeshift/TradeshiftApplication.java
+++ src/main/java/org/group10/tradeshift/TradeshiftApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TradeshiftApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TradeshiftApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TradeshiftApplication.class, args);
+    }
 
 }
